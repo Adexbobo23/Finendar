e68c0ad78079a7577a99b52d0b32781e841830b5
Adeoluwa <Oliyideadeoluwajohn1@gmail.com>
2024-06-08 14:40:24 +0100

    Added some corrections

--- courses/views.py
+++ courses/views.py
@@ -7,16 +7,19 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='login')
 def create_project(request):
     if request.method == 'POST':
-        form = CourseForm(request.POST)
+        form = CourseForm(request.POST, request.FILES)
         if form.is_valid():
             course_instance = form.save(commit=False)
-            course_instance.user = request.user 
+            course_instance.user = request.user
             course_instance.save()
             return redirect('instructors_dashboard')
+        else:
+            print(form.errors) 
     else:
         form = CourseForm()
-    
+
     return render(request, 'dashboard/create-course.html', {'form': form})
+
 
 
 def all_courses(request):
