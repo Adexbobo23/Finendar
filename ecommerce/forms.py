93a71bdba63d1a76f4e7ebfa7d46e2e2db719d65
Adeoluwa <Oliyideadeoluwajohn1@gmail.com>
2024-06-28 13:31:58 +0100

    More functions is Added

--- ecommerce/forms.py
+++ ecommerce/forms.py
@@ -4,4 +4,4 @@ from .models import Product
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['title', 'description', 'price', 'discounted_price', 'category', 'main_image', 'image1', 'image2', 'image3', 'image4', 'video', 'stock', 'available']
+        fields = ['title', 'description', 'tags', 'price', 'discounted_price', 'category', 'main_image', 'image1', 'image2', 'image3', 'image4', 'video', 'stock', 'available']
